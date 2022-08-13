af54067d90c2686fe04f77f3431d8ed3a403d7b4
joyjunyi <109071362+joyjunyi@users.noreply.github.com>
2022-08-13 15:44:42 +0100

    Update LinkedList.java

--- dataStructure/LinkedList.java
+++ dataStructure/LinkedList.java
@@ -1,4 +1,40 @@
 package dataStructure;
-
+/*
+完成对链表增删改查的功能
+ */
 public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
+//定义SingleLinkedList，来管理Student
+class SingleLinkedList{
+    //先初始化一个 Head，头节点不要动，不存放具体数据
+    private Student head = new Student(0,"","");
+
+    //添加节点到单向链表
+    
+}
+//定义Student,每个Student对象就是一个节点
+class Student{
+    public int no;
+    public String name;
+    public String nickname;
+    public Student next;    //指向下一个节点
+    //构造器
+    public Student(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
+    //为了方便显示，重写toString
+    @Override
+    public String toString() {
+        return "Student{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", next=" + next +
+                '}';
+    }
 }
