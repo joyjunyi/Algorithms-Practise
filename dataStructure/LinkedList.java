e7e8acdc96f44fe9bf085ea33f8f61babe0dab9d
joyjunyi <109071362+joyjunyi@users.noreply.github.com>
2022-08-15 14:57:16 +0100

    update

--- dataStructure/LinkedList.java
+++ dataStructure/LinkedList.java
@@ -4,7 +4,21 @@ package dataStructure;
  */
 public class LinkedList {
     public static void main(String[] args) {
+        //创建节点
+        Student student01 = new Student(01,"张三","小张");
+        Student student02 = new Student(02,"玛丽","小马");
+        Student student03 = new Student(03,"戴西","小戴");
+        Student student04 = new Student(04,"王五","小王");
 
+        //创建一个链表，然后加入
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.addByOrder(student01);
+        singleLinkedList.addByOrder(student04);
+        singleLinkedList.addByOrder(student02);
+        singleLinkedList.addByOrder(student03);
+
+        //显示
+        singleLinkedList.traverse();
     }
 }
 //定义SingleLinkedList，来管理Student
@@ -29,8 +43,63 @@ class SingleLinkedList{
         //此时连上最后一个节点即可
         temp.next = student;    //形参列表里新加入的节点
     }
-    // 验证一下，显示链表，
-    // 通过遍历显示所有节点，也需要借助辅助变量
+    //按照序号将新节点插入到链表
+    //如果序号重复，请提示
+    public void addByOrder(Student student){
+        Student temp = head;    //我们要找的位置的新节点前一个，所以这里的temp不是等于head.next
+        boolean flag = false;   //添加的编号是否存在
+        while(true){
+            if(temp.next == null){  //说明已经到链表最后一位
+                break;      //这时不管找到没找到都要break
+            }
+            if(temp.next.id > student.id){  //位置找到，插入到temp后面
+                break;
+            }else if(temp.next.id == student.id){ //编号已存在
+                flag = true;
+                break;
+            }
+            temp = temp.next;   //后移（遍历）
+        }
+        //判断flag的值
+        if(flag){
+            System.out.println("此学生编号"+student.id+"已存在");
+        }else{
+            //插入到链表中，temp的后面
+            student.next = temp.next;
+            temp.next = student;
+        }
+    }
+    //修改节点
+    public void update(Student updatedStudent){
+        Student temp = head.next;
+        //先判断是否为空
+        if(temp == null){
+            System.out.println("链表为空");
+            return;
+        }
+        //新建标识，记录节点是否被找到
+        boolean flag = false;
+        while(true){
+            if(temp == null) {
+                break;      //表示遍历完链表了，
+            }
+            if(temp.id == updatedStudent.id){   //找到了
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //根据flag判断是否找到
+        if(flag){
+            temp.name = updatedStudent.name;
+            temp.nickname = updatedStudent.nickname;
+        }else{  //没有找到
+            System.out.println("没有找到编号为"+updatedStudent.id+"的节点，无法修改");
+        }
+    }
+
+    // 打印链表
+    // 通过遍历打印所有节点，也需要借助辅助变量
     public void traverse(){
         //检查链表是否为空，为空就不遍历了
         if(head.next == null){
@@ -41,31 +110,38 @@ class SingleLinkedList{
         //因为头结点不能动，索引需要一个辅助变量
         Student temp = head.next;
         while(true){    //造个死循环
-            
-
+            //判断是否到链表最后
+            if(temp == null){
+                break;
+            }
+            //没有到最后的话，输出链表
+            //因为Student类已经重写toString方法了，因此直接打印即可
+            System.out.println(temp);
+            //temp后移
+            temp = temp.next;
         }
     }
 }
 //定义Student,每个Student对象就是一个节点
 class Student{
-    public int no;
+    public int id;
     public String name;
     public String nickname;
     public Student next;    //指向下一个节点
     //构造器
-    public Student(int no, String name, String nickname) {
-        this.no = no;
+    public Student(int id, String name, String nickname) {
+        this.id = id;
         this.name = name;
         this.nickname = nickname;
     }
     //为了方便显示，重写toString
+    //自动生成的toString记得把next部分去了，否则会连续输出所有节点
     @Override
     public String toString() {
         return "Student{" +
-                "no=" + no +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 ", nickname='" + nickname + '\'' +
-                ", next=" + next +
                 '}';
     }
 }
